adc74d9dd1c14981edd825c124440360a0409f78
Zicheng S <54545010+zshi74@users.noreply.github.com>
2020-02-02 12:06:34 -0500

    medium

--- Database/534-Game Play Analysis III.sql
+++ Database/534-Game Play Analysis III.sql
@@ -1,8 +1,8 @@
 # Write your MySQL query statement below
 # 534. Game Play Analysis III
 
-SELECT A2.player_id, A2.event_date, SUM(A1.games_played) AS games_played_so_far
-FROM Activity AS A1 
-INNER JOIN Activity AS A2 ON (A1.player_id = A2.player_id 
-                              AND A1.event_date <= A2.event_date)
-GROUP BY A2.player_id, A2.event_date;
+SELECT a1.player_id, a1.event_date, sum(a2.games_played) games_played_so_far
+FROM Activity a1
+INNER JOIN Activity a2 ON (a1.player_id = a2.player_id
+                          AND a1.event_date >= a2.event_date)
+GROUP BY a1.player_id, a1.event_date
