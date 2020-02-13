56ae839f48717d85c3e3ebb4f0f4d5cc104b5585
Zicheng(Stone) Shi <54545010+zshi74@users.noreply.github.com>
2020-02-13 00:53:31 -0500

    Add files via upload

---
+++ Database/586-Customer Placing the Largest Number of Orders.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+# 586. Customer Placing the Largest Number of Orders
+SELECT customer_number
+FROM orders
+GROUP BY customer_number 
+ORDER BY COUNT(*) DESC
+LIMIT 1
