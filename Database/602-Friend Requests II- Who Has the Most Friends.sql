a48547345840782820af102525f7f26a66dc381b
Zicheng(Stone) Shi <54545010+zshi74@users.noreply.github.com>
2020-02-15 00:54:20 -0500

    Add files via upload

---
+++ Database/602-Friend Requests II- Who Has the Most Friends.sql
@@ -0,0 +1,12 @@
+## 602. Friend Requests II: Who Has the Most Friends
+
+select ids as id, cnt as num
+from (select ids, count(*) as cnt
+      from (
+        select requester_id as ids from request_accepted
+        union all
+        select accepter_id from request_accepted
+     ) as tbl1
+group by ids) as tbl2
+order by cnt desc
+limit 1
